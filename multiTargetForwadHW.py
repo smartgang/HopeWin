bddc6b917527d67204da9fe31cb8d6d7627e7499
Smart <smartgang@126.com>
2018-03-16 14:42:11 +0800

    [MOD]修改参数设置方式

--- multiTargetForwadHW.py
+++ multiTargetForwadHW.py
@@ -18,11 +18,24 @@ if __name__=='__main__':
     nextmonth = 'Jan-18'
     # windowsSet=[1,2,3,4,5,6,9,12,15]
     windowsSet = range(1, 13)  # 白区窗口值
-
-    newresult = True #！！正常:False，双止损:True
-    resultfilesuffix = 'resultDSL_by_tick.csv'  # 前面不带空格,正常:result.csv,dsl:resultDSL_by_tick.csv,ownl:resultOWNL_by_tick.csv
+    #双止损开关和参数设置
+    dsl=True
+    ownl=False
+    dslownl=False
+    dslTarget=-0.022
+    ownlTarget=0.009
+    #=============================================================================================================
+    #newresult = True #！！正常:False，双止损:True
+    if dsl:
+        resultfilesuffix = 'resultDSL_by_tick.csv'  # 前面不带空格,正常:result.csv,dsl:resultDSL_by_tick.csv,ownl:resultOWNL_by_tick.csv
+    elif ownl:
+        resultfilesuffix = 'resultOWNL_by_tick.csv'  # 前面不带空格,正常:result.csv,dsl:resultDSL_by_tick.csv,ownl:resultOWNL_by_tick.csv
+    elif dslownl:
+        resultfilesuffix= 'result_dsl_ownl.csv'
+    else:
+        resultfilesuffix='result.csv'
     monthlyretrsuffix = 'monthly_retr_new.csv'  # 前面不带下划线,正常:monthly_retr.csv,双止损:monthly_retr_new.csv
-    colslist = mtf.getColumnsName(newresult)
+    colslist = mtf.getColumnsName(dsl or ownl or dslownl)
 
     # ============================================文件路径========================================================
     upperpath = DC.getUpperPath(uppernume=1)
@@ -34,7 +47,16 @@ if __name__=='__main__':
     setlist = pd.Series([str(K_MIN_MACD)+' '] * parasetlist.shape[0])   #因为HP的文件名跟LW不同，多了一个MACD的周期在前面，要处理一下
     parasetlist['Setname'] = setlist.str.cat(parasetlist['Setname'])
 
-    rawdatapath = resultpath + foldername + '\\DynamicStopLoss-22.0\\' #！！正常:'\\'，双止损：填上'\\+双止损目标文件夹\\'
+    if dsl:
+        rawdatapath = resultpath + foldername + "\\DynamicStopLoss" + str(dslTarget*1000)+'\\'  # ！！正常:'\\'，双止损：填上'\\+双止损目标文件夹\\'
+    elif ownl:
+        rawdatapath = resultpath + foldername + "\\OnceWinNoLoss" + str(ownlTarget*1000)+'\\'  # ！！正常:'\\'，双止损：填上'\\+双止损目标文件夹\\'
+    elif dslownl:
+        newfolder = ("\\dsl_%.3f_ownl_%.3f\\" % (dslTarget, ownlTarget))
+        rawdatapath = resultpath + foldername + newfolder  # ！！正常:'\\'，双止损：填上'\\+双止损目标文件夹\\'
+    else:
+        rawdatapath = resultpath + foldername+'\\'
+
     forwordresultpath = rawdatapath + '\\ForwardResults\\'
     forwardrankpath = rawdatapath + '\\ForwardRank\\'
 
@@ -57,19 +79,19 @@ if __name__=='__main__':
     starttime = datetime.now()
     print starttime
     # 多进程优化，启动一个对应CPU核心数量的进程池
-    '''
+
     pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
     l = []
     for whiteWindows in windowsSet:
-        #l.append(mtf.runPara(whiteWindows, symbol, K_MIN_SAR, parasetlist, monthlist, rawdatapath, forwordresultpath, forwardrankpath, colslist, resultfilesuffix))
-        l.append(pool.apply_async(mtf.runPara, (
-        whiteWindows, symbol, K_MIN_SAR, parasetlist, monthlist, rawdatapath, forwordresultpath, forwardrankpath, colslist,
-        resultfilesuffix)))
+        l.append(mtf.runPara(whiteWindows, symbol, K_MIN_SAR, parasetlist, monthlist, rawdatapath, forwordresultpath, forwardrankpath, colslist, resultfilesuffix))
+        #l.append(pool.apply_async(mtf.runPara, (
+        #whiteWindows, symbol, K_MIN_SAR, parasetlist, monthlist, rawdatapath, forwordresultpath, forwardrankpath, colslist,
+        #resultfilesuffix)))
     pool.close()
     pool.join()
 
     mtf.calGrayResult(symbol, K_MIN_SAR, windowsSet, forwardrankpath, rawdatapath, monthlyfilesuffix=monthlyretrsuffix)
-    '''
+
     mtf.calOprResult(rawdatapath, symbol, K_MIN_SAR, nextmonth, columns=colslist, resultfilesuffix=resultfilesuffix)
     endtime = datetime.now()
     print starttime
