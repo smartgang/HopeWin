0b68677a67c8c1ce8dad3388ba94dc1ba9d1f22c
Smart <smartgang@126.com>
2018-03-16 09:40:28 +0800

    [MOD]修改进场过滤条件

--- HopeWin.py
+++ HopeWin.py
@@ -106,8 +106,11 @@ def HopeWin(symbolInfo,setname,K_MIN_SAR,K_MIN_MACD,startdate,enddate,macdParaSe
     shortcrosslist = shortcrosslist.set_index(pd.Index(shortcrosslist['deathcrossindex']), drop=True)
 
     # 取出开多序号和开空序号
-    openlongindex = rawdata_sar.loc[(rawdata_sar['SAR_R'] == 1) & (rawdata_sar['MACD_True'] == 1)].index
-    openshortindex = rawdata_sar.loc[(rawdata_sar['SAR_R'] == -1) & (rawdata_sar['MACD_True'] == -1)].index
+    #SAR金叉做多，死叉做空
+    #做多过滤：MACD处于金叉期间且DIF>=0
+    #做空过滤：MACD处于死叉期间且DIF<0
+    openlongindex = rawdata_sar.loc[(rawdata_sar['SAR_R'] == 1) & (rawdata_sar['MACD_True'] == 1) & (rawdata_sar['DIF'] >= 0)].index
+    openshortindex = rawdata_sar.loc[(rawdata_sar['SAR_R'] == -1) & (rawdata_sar['MACD_True'] == -1) & (rawdata_sar['DIF'] < 0)].index
 
     # 从多仓序列中取出开多序号的内容，即为开多操作
     longopr = longcrosslist.loc[openlongindex]
